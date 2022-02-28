d75692bf335afe3d84d9588a22be9ba5b0965541
abc-xyz1 <61141871+abc-xyz1@users.noreply.github.com>
2022-02-28 16:45:43 +0800

    Add files via upload

---
+++ readCkpt.py
@@ -0,0 +1,36 @@
+import os
+
+from tensorflow.python import pywrap_tensorflow
+import tensorflow as tf
+
+current_path = os.getcwd()
+model_dir = os.path.join('', '') # path contains SIFA model
+checkpoint_path = os.path.join(model_dir,'sifa-cardiac-ct2mr')
+new_checkpoint_path = '' # path to save new model
+os.environ['CUDA_VISIBLE_DEVICES'] = '5'
+
+
+# Read data from checkpoint file
+reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
+var_to_shape_map = reader.get_variable_to_shape_map()
+# Print tensor name and values
+
+
+new_var_list = [] 
+for key in var_to_shape_map:
+    #print("tensor_name: ", key)
+    n = key
+    v = reader.get_tensor(key)
+    # choose model parameters of g_A and d_B
+    if "/g_A/" in key or "/d_B/" in key:
+        print("tensor_name: ", key)
+        named_var = tf.Variable(v, name=n)
+        new_var_list.append(named_var)
+    
+saver = tf.train.Saver(var_list=new_var_list)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    model_name = 'ct2mr' 
+    checkpoint_path = os.path.join(new_checkpoint_path, model_name) 
+    saver.save(sess, checkpoint_path) 
+    print("done !")
